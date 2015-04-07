445140c26483f3d8dbfee36b1866b8e493ea7ff8
Ben Cheng & Lauren N. Roth <pair+bcheng90.laurennicoleroth@devbootcamp.com>
2015-04-07 10:54:18 -0400

    Test user email is unique and fits valid regex pattern. 	modified:   app/models/user.rb 	modified:   test/models/user_test.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :first_name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
