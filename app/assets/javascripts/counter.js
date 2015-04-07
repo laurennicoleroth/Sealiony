56a965bb872c9290c9459c996c9e38cbde67d4a7
Ben Cheng & Lauren N. Roth <pair+bcheng90.laurennicoleroth@devbootcamp.com>
2015-04-07 16:44:50 -0400

    Add initial ajax for incrementing click counter. 	new file:   app/assets/javascripts/counter.js

---
+++ app/assets/javascripts/counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".short_url").on('click', function(){
+    var request = {
+      url: target.action,
+      method: 'count',
+    }
+    $.ajax(request).done;
+  });
+
+
+
+
+});
