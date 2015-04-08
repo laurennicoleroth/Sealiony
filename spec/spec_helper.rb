c5417966da351bd46a49493369592e0a6dbaa7c1
Ben Cheng & Lauren N. Roth <pair+bcheng90.laurennicoleroth@devbootcamp.com>
2015-04-08 16:37:54 -0400

    Comment out database cleaning so that we can accrue urls. modified:   spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,20 +43,20 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with = (:truncation)
-  end
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
 
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
+  # config.before(:each, :js => true) do
+  #   DatabaseCleaner.strategy = :truncation
+  # end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  # config.after(:each) do
+  #   # DatabaseCleaner.clean
+  # end
 end
